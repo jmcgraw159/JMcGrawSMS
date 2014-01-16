13b70d79e779bf5725d9fb6fd697890ffb7ed71d
Jeremy McGraw <jmcgraw159@fullsail.edu>
2014-01-16 16:13:02 -0500

    Added lab 3 and updated jquery

--- Site/js/main.js
+++ Site/js/main.js
@@ -1,76 +1,130 @@
-$(document).ready(function(){
+var connected = function(success, error){
 
-  $('.bxslider').bxSlider({
-	controls: false,
-	preloadImages: 'visible',
-	touchEnabled: true,
-	oneToOneTouch: true,
-	auto: true
-  });
-
-});
-
-var loginNav = $('.nav');
-loginNav.hide();
-
-$('.login').on('mouseenter', function(e){
-	e.preventDefault();
-	loginNav.show();
-});
-
-loginNav.on('mouseleave', function(e){
-	e.preventDefault();
-	loginNav.hide();
-});
-
-var volumeBG = $('.vcBG');
-volumeBG.hide();
-
-$('.volume').on('mouseenter', function(e){
-	e.preventDefault();
-	volumeBG.show();
-});
-
-volumeBG.on('mouseleave', function(e){
-	e.preventDefault();
-	volumeBG.hide();
-});
-
-var play = true;
-$('.play').on('click', function(e){
-	e.preventDefault();
-
-	if(play){
-		$('.play').attr('src', 'img/pause.png');
-		play = false;
+	if(success){
+		console.log('Success');
+		flash.startPlaying('hobbit_vp6.flv');
 	}else {
-		$('.play').attr('src', 'img/play.png');
-		play = true;
+		console.log(error)
 	}
-});
+}
 
-var micBG = $('.micBG');
-micBG.hide();
+var flashReady = function(){
 
-$('.microphone').on('mouseenter', function(e){
-	e.preventDefault();
-	micBG.show();
-});
+	console.log('Flash Ready');
 
-micBG.on('mouseleave', function(e){
-	e.preventDefault();
+	var loginNav = $('.nav');
+	loginNav.hide();
+
+	$('.login').on('mouseenter', function(e){
+		e.preventDefault();
+		loginNav.show();
+	});
+
+	loginNav.on('mouseleave', function(e){
+		e.preventDefault();
+		loginNav.hide();
+	});
+
+	var volumeBG = $('.vcBG');
+	volumeBG.hide();
+
+	$('.volume').on('mouseenter', function(e){
+		e.preventDefault();
+		volumeBG.show();
+	});
+
+	volumeBG.on('mouseleave', function(e){
+		e.preventDefault();
+		volumeBG.hide();
+	});
+
+	var play_toggle = true;
+	var play = false;
+	$('.play').on('click', function(e){
+		e.preventDefault();
+
+		if(!play){
+			flash.connect('rtmpt://localhost:5080/SMServer/');
+			play = true;
+		}else {
+			flash.playPause();
+		}
+
+		if(play_toggle){
+			$('.play').attr('src', 'img/pause.png');
+			play_toggle = false;
+		}else {
+			$('.play').attr('src', 'img/play.png');
+			play_toggle = true;
+		}
+	});
+
+	var micBG = $('.micBG');
 	micBG.hide();
-});
 
-var camBG = $('.camBG');
-camBG.hide();
+	var microphones = flash.getMicrophones();
+	microphoneIndex = microphones[0];
+	console.log(microphoneIndex);
+	for(var i = 0; i < microphones.length; i++){
+		$('.micBG').append('<a href="#" class="mic">' + microphones[i] + '</a>');
+	}
 
-$('.camera').on('mouseenter', function(e){
-	e.preventDefault();
-	camBG.show();
-});
+	$('.microphone').on('mouseenter', function(e){
+		e.preventDefault();
+		micBG.show();
+	});
 
-camBG.on('mouseleave', function(e){
-	e.preventDefault();
+	micBG.on('mouseleave', function(e){
+		e.preventDefault();
+		micBG.hide();
+	});
+
+	$('.mic').on('click', function(e){
+		e.preventDefault();
+		console.log(event.currentTarget);
+	})
+
+	var camBG = $('.camBG');
 	camBG.hide();
-});
+
+	var cameras = flash.getCameras();
+	cameraIndex = cameras[0];
+	console.log(cameraIndex);
+	for(var i = 0; i < cameras.length; i++){
+		$('.camBG').append('<a href="#" class="cam">' + cameras[i] + '</a>');
+	}
+
+	$('.camera').on('mouseenter', function(e){
+		e.preventDefault();
+		camBG.show();
+	});
+
+	camBG.on('mouseleave', function(e){
+		e.preventDefault();
+		camBG.hide();
+	});
+
+	$('.cam').on('click', function(e){
+		e.preventDefault();
+		console.log(event.currentTarget);
+	});
+
+	filename = 'this_is_a_test';
+
+	$('.record').on('click', function(e){
+		flash.startRecording(filename, cameraIndex, microphoneIndex);
+	});
+
+}
+
+// $(document).ready(function(){
+
+//   $('.bxslider').bxSlider({
+// 	controls: false,
+// 	preloadImages: 'visible',
+// 	touchEnabled: true,
+// 	oneToOneTouch: true,
+// 	auto: true
+//   });
+
+// });
